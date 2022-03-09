023eb129e9d9749aaeef023c1c680175d3b69887
wackyjoker <kevin.young8931@gmail.com>
2022-03-09 23:43:51 +0100

    setting Card layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import Card from '@/layouts/Card'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
@@ -23,16 +23,8 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+        <Card/>
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
             className={styles.card}
