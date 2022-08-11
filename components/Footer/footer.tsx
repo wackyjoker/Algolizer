09962f7c867587345fdb397508039d3eb7f641b9
wackyjoker <kevin.young8931@gmail.com>
2022-08-11 18:11:02 +0200

    footer placeholder component

---
+++ components/Footer/footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+// interface IAppProps {
+// }
+
+const Footer:React.FunctionComponent = () => (
+  <div>
+    <p>this is a footer</p>
+  </div>
+)
+
+export default Footer
